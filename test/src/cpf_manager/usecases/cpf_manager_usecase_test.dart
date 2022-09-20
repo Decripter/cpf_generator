11d0d1cc53f7d5a7fb11fd28a15317c1cc18a718
Decripter <jeilsonaraujo@icloud.com>
2022-09-20 20:02:25 -0300

    test: adding a test for usecase cpf_manager

--- test/src/cpf_manager/usecases/cpf_manager_usecase_test.dart
+++ test/src/cpf_manager/usecases/cpf_manager_usecase_test.dart
@@ -1,8 +1,8 @@
-import 'package:cpf_generator/src/cpf_manager/services/cpf_manager_service.dart';
+import 'package:cpf_generator/src/cpf_manager/usecases/cpf_manager_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  ICpfManager cpfManager = CpfManager();
+  CpfManager cpfManager = CpfManager();
   test('shoud return false if is empty', () {
     var isValid = cpfManager.cpfValidator('');
 
