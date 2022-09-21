a32f65548fedbfa096de97d0456bd4f989f8cfd9
Decripter <jeilsonaraujo@icloud.com>
2022-09-20 21:48:14 -0300

    feat: finish some features on cpfValidator function

--- lib/src/cpf_manager/usecases/cpf_manager_usecase.dart
+++ lib/src/cpf_manager/usecases/cpf_manager_usecase.dart
@@ -1,15 +1,39 @@
 class CpfManager {
-  @override
+  //
+
   String cpfGenerator() {
-    // TODO: implement cpfGenerator
-    throw UnimplementedError();
+    return '';
   }
 
-  @override
   bool cpfValidator(String cpf) {
-    if (cpf.isEmpty) {
+    //
+    if (cpf.isEmpty || cpf.length != 11) {
       return false;
     }
-    return true;
+
+    var validatorDigit = '';
+    var validatorDigitOriginal = cpf.substring(9, 11);
+
+    validatorDigit =
+        '$validatorDigit${_getValidatorDigit(cpf: cpf, digitsForLoop: 8)}';
+    validatorDigit =
+        '$validatorDigit${_getValidatorDigit(cpf: cpf, digitsForLoop: 9)}';
+
+    return (int.parse(validatorDigit) == int.parse(validatorDigitOriginal));
+  }
+
+  int _getValidatorDigit({required String cpf, required int digitsForLoop}) {
+    var cpfSum = 0;
+    var mult = 2;
+    for (int digit = digitsForLoop; digit >= 0; digit--) {
+      cpfSum = (int.parse(cpf[digit]) * mult) + cpfSum;
+      mult++;
+    }
+
+    if (cpfSum % 11 < 2) {
+      return 0;
+    } else {
+      return 11 - (cpfSum % 11);
+    }
   }
 }
